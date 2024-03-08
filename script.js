73e7357c5ec92822a64a1bd36d43f53b4090bb1e
Hunub <143363415+Hunub@users.noreply.github.com>
2024-03-08 16:42:15 +1100

    Refind styling Added two fonts Adjust text layout for text-container class Added increment/decrement page number function

--- script.js
+++ script.js
@@ -25,10 +25,12 @@ createBtn.addEventListener("click", ()=>{
 });
 
 
-function Book(title,author,read){
+function Book(title,author,totalPage, currentPage){
     this.title = title;
     this.author = author;
-    this.read = readToBoolean(read);
+    this.totalPage = totalPage;
+    this.currentPage = currentPage;
+    this.read = this.totalPage === this.currentPage? true: false;
 };
 
 function creatNewBook(event){
@@ -37,9 +39,11 @@ function creatNewBook(event){
     let i= myLibrary.length;
     let title_i = document.getElementById("title").value;
     let author_i = document.getElementById("author").value;
-    let read_i = document.querySelector("input[name='state']:checked").value;
+    let totalPage_i = document.getElementById("total-page").value;
+    let currentPage_i = document.getElementById("cur-page").value;
+    // let read_i = document.querySelector("input[name='state']:checked").value;
 
-    myLibrary[i] = new Book(title_i,author_i,read_i)
+    myLibrary[i] = new Book(title_i,author_i,totalPage_i,currentPage_i);
 
     renderLibrary(myLibrary);
 };
@@ -50,7 +54,10 @@ function renderLibrary(array){
     for(i=0; i<l; i++){
         renderBook(i);
     };
+    console.log(myLibrary);
 };
+
+
 
 function renderBook(i){
 
@@ -64,6 +71,7 @@ function renderBook(i){
     library.appendChild(bookContainer);
 
     const textContainer = document.createElement("div");
+    textContainer.setAttribute('class','text-container');
     bookContainer.appendChild(textContainer);
 
     const bookTitle = document.createElement("h2");
@@ -76,6 +84,46 @@ function renderBook(i){
     bookAuthor.innerText = myLibrary[i].author;
     textContainer.appendChild(bookAuthor);
 
+    const curPageText = document.createElement("p");
+    curPageText.setAttribute('class','book-cur-page');
+    curPageText.innerText = myLibrary[i].currentPage;
+    
+    const totalPageText = document.createElement("p");
+    totalPageText.setAttribute('class','book-total-page');
+
+    const banner = document.createElement("h3");
+    banner.setAttribute('class','book-banner');
+    banner.innerText = "Finished";
+    
+    if(myLibrary[i].read === true){
+        totalPageText.innerText = `Number of Pages: ${myLibrary[i].totalPage}`;
+        textContainer.appendChild(totalPageText);
+        bookContainer.appendChild(banner);
+
+    }else{
+        const pageNumber = document.createElement("div");
+        pageNumber.setAttribute('class','page-container');
+        const curPageInc = document.createElement("div");
+        curPageInc.addEventListener("click", incrementPage);
+        curPageInc.setAttribute('class','book-inc')
+        const curPageDec = document.createElement("div");
+        curPageDec.addEventListener("click", decrementPage);
+        curPageDec.setAttribute('class','book-dec')
+
+        const slash = document.createElement("p");
+        slash.setAttribute('class','slash');
+        slash.innerText = "/";
+        
+        totalPageText.innerText = myLibrary[i].totalPage;
+        textContainer.appendChild(pageNumber);
+        pageNumber.appendChild(curPageText);
+        pageNumber.appendChild(curPageDec);
+        pageNumber.appendChild(curPageInc);
+        pageNumber.appendChild(slash);
+        pageNumber.appendChild(totalPageText);
+    };
+
+
     const deleteBtn= document.createElement("button");
     deleteBtn.setAttribute('class','book-delete');
     deleteBtn.innerHTML= '<span class="material-symbols-outlined">close</span>';
@@ -85,7 +133,6 @@ function renderBook(i){
     const toggleReadBtn = document.createElement("button");
     toggleReadBtn.setAttribute('class','book-read');
     toggleReadBtn.innerHTML= '<span class="material-symbols-outlined">check</span>';
-    // toggleReadBtn.innerText = toggleReadDisplay(myLibrary[i]);
     toggleReadBtn.addEventListener("click", toggleRead);
     bookContainer.appendChild(toggleReadBtn);
 };
@@ -96,6 +143,26 @@ function readToBoolean(value){
 
 function toggleReadDisplay(item){
     return item.read ? "Read" : "Not read";
+};
+
+function incrementPage(event){
+    let i = event.target.parentNode.parentNode.parentNode.getAttribute("index");
+    if(myLibrary[i].currentPage > myLibrary[i].totalPage - 1){
+        return;
+    }else{
+        myLibrary[i].currentPage++;
+        event.target.parentNode.querySelector("p").innerText = myLibrary[i].currentPage;
+    };
+};
+
+function decrementPage(event){
+    let i = event.target.parentNode.parentNode.parentNode.getAttribute("index");
+    if(myLibrary[i].currentPage < 1){
+        return;
+    }else{
+        myLibrary[i].currentPage--;
+        event.target.parentNode.querySelector("p").innerText = myLibrary[i].currentPage;
+    };
 };
 
 function deleteBook(event){
@@ -112,11 +179,11 @@ function toggleRead(event){
     renderLibrary(myLibrary);
 }
 
-myLibrary[0] =  new Book("Moby Dick","Herman Melville","read");
-myLibrary[1] =  new Book("The Count of Monte Cristo","Alexandre Dumas","read");
-myLibrary[2] =  new Book("Don Quixote","Miguel de Cervantes","not-read");
-myLibrary[3] =  new Book("The Courage to be Disliked: The Japanese phenomenon that shows you how to free yourself, change your life and achieve real happiness","Ichiro Kishimi,Fumitake Koga","read");
-myLibrary[4] =  new Book("Walden","Henry David Thoreau","not-read");
-myLibrary[5] =  new Book("The Art of Loving","Erich Fromm","not-read");
+myLibrary[0] =  new Book("Moby Dick","Herman Melville","210","210");
+myLibrary[1] =  new Book("The Count of Monte Cristo","Alexandre Dumas","212","212");
+myLibrary[2] =  new Book("Don Quixote","Miguel de Cervantes","130","21");
+myLibrary[3] =  new Book("The Courage to be Disliked: The Japanese phenomenon that shows you how to free yourself, change your life and achieve real happiness","Ichiro Kishimi,Fumitake Koga","155","155");
+myLibrary[4] =  new Book("Walden","Henry David Thoreau","178","12");
+myLibrary[5] =  new Book("The Art of Loving","Erich Fromm","3","1");
 
 renderLibrary(myLibrary);
